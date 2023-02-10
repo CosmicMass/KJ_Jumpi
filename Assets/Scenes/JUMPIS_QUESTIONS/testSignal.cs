79646c0330e8ea548df34eabef12a01891801552
Mehmet Pala <mehmetapostrof@gmail.com>
2023-02-10 18:15:30 +0300

    asd

---
+++ Assets/Scenes/JUMPIS_QUESTIONS/testSignal.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class testSignal : MonoBehaviour
+{
+    public void TESTSignal()
+    {
+        print("helloo");
+        
+    }
+}
